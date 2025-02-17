5e93379dd3fd8317aaf2e1a8514527226a098c60
unknown <jaybhuptani1054@gmail.com>
2025-02-17 20:07:47 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="h-screen w-full">
+      <p className="text-white">hy</p>hello
+    </div>
+  );
+};
+
+export default App;
