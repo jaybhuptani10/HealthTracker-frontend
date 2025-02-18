80a7a4ed6db33d8d14b596fc7ab53009e20cb83e
unknown <jaybhuptani1054@gmail.com>
2025-02-18 12:52:32 +0530

    Walking

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Stepcounter from "./Stepcounter";
-
+import { RadialBarChart, RadialBar, PolarAngleAxis } from "recharts";
 import StepCounterComp from "../components/StepCounterComp";
 
 const Home = () => {
+  const data = [{ name: "Score", value: 75, fill: "#8884d8" }];
   return (
     <div className="min-h-screen w-full p-10 flex">
       <Navbar />
--- src/components/StepCounterComp.jsx
+++ src/components/StepCounterComp.jsx
@@ -1,11 +1,41 @@
 import React from "react";
+import { RadialBarChart, RadialBar, PolarAngleAxis } from "recharts";
 
 const StepCounterComp = () => {
-  const [steps, setSteps] = React.useState(0);
+  const data = [{ name: "Score", value: 75, fill: "#8338EC" }];
   return (
-    <div
-      className={`h-48 w-48 rounded-full border-5 border-gray-500 border-b-transparent`}
-    ></div>
+    <div className="h-66 w-66 bg-[#EAF1F3] flex items-center justify-center relative rounded-2xl cursor-pointer">
+      <RadialBarChart
+        className="absolute"
+        width={500}
+        height={400}
+        cx={240}
+        cy={210}
+        innerRadius={210}
+        outerRadius={220}
+        startAngle={220}
+        endAngle={-40}
+        barSize={15}
+        data={data}
+      >
+        <PolarAngleAxis
+          type="number"
+          domain={[0, 100]}
+          angleAxisId={0}
+          tick={false}
+        />
+        <RadialBar
+          minAngle={15}
+          background={{ fill: "#DCE2E5" }}
+          clockWise
+          dataKey="value"
+        />
+      </RadialBarChart>
+
+      <h1 className="absolute text-4xl">8,000</h1>
+      <span className="absolute text-xl text-gray-500 mt-16">Steps</span>
+      <h1 className="absolute bottom-10 text-xl text-gray-500">10,000 steps</h1>
+    </div>
   );
 };
 
